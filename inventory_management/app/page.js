84d32e65e3abddf08bef17a11b1dc11b5e035ccd
Chang Oh <72629686+chang4oh@users.noreply.github.com>
2024-08-01 17:38:07 -0500

    29:10 adding button to the textbox

--- inventory_management/app/page.js
+++ inventory_management/app/page.js
@@ -1,44 +1,125 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/Firebase";
-import { Box, Typography } from "@mui/material";
-
+import {
+  Box,
+  Modal,
+  Typography,
+  Stack,
+  TextField,
+  Button,
+} from "@mui/material";
+import { collection, deleteDoc, doc, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
-  const [inventory, setInventory] = useState([])
-  const [open, setOpen] = useState([false])
-  const [itemName, setItemName] = useState(['']) /* default value empty string */
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState([false]);
+  const [itemName, setItemName] = useState([
+    "",
+  ]); /* default value empty string */
 
   const updateInventory = async () => {
-    const snapshot = query(collection(firestore, 'inventory'))
-    const docs = await getDocs(snapshot)
-    const inventoryList = []
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
     docs.forEach((doc) => {
       inventoryList.push({
         name: doc.id,
         ...doc.date(),
-      })
-    })
-    setInventory(inventoryList)
-  }
+      });
+    });
+    setInventory(inventoryList);
+    console.log(inventoryList);
+  };
 
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
 
-  useEffect(() => { /* update when page loads */
-    updateInventory()
-  }, [])
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity - 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+
+    await updateInventory();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+
+    await updateInventory();
+  };
+
+  useEffect(() => {
+    /* update when page loads */
+    updateInventory();
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
-    <Box>
-      <Typography variant='h1'>Inventory Management</Typography>
-      {
-        inventory.forEach((item) => {
-          return (<>
-            {item.name}
-            {item.count}
-          </>)
-        })
-      }
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems={"center"}
+      gap={2}
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%,-50%)",
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value);
+              }}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName);
+                setItemName("");
+                handleClose();
+              }}
+            ></Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Typography variant="h1">Inventory Management</Typography>
     </Box>
-  )
+  );
 }
